d53670ac72d8b1133ea907c02376a4e936100f1e
Natalia Litvinok <minilampa@gmail.com>
2019-03-05 23:34:35 +0300

    feat: add solution for Love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let arr = preferences;
+  let result = 0;
+
+  function chekTriangl(i) {
+    let first = arr[i] - 1;
+    let second = arr[first] && arr[first] - 1 !== first ? arr[first] - 1 : null;
+    let third = arr[second] && arr[second] - 1 !== second ? arr[second] - 1 : null;
+
+    if (third == i) {
+      arr[i] = arr[first] = arr[second] = 0;
+      return true;
+    }
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (chekTriangl(i)) {
+      result++;
+    }
+  }
+  return result;
 };
